4f4dcde715c1304d267bcb6fcd017c799fe5717e
Crowdbotics <team@crowdbotics.com>
2019-04-05 17:34:34 +0000

    initial commit

---
+++ android/app/src/main/java/com/hotbox_1985/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hotbox_1985;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "hotbox_1985";
+    }
+}
